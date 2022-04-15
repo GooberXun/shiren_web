ce52298c208a1f2b50f4c4a4f71966b00ec70dca
Goober <xunxiaobo2016@163.com>
2022-04-15 09:26:24 +0800

    第三次提交

--- src/main/java/com/dongao/phoenix/shiren/controller/IndexController.java
+++ src/main/java/com/dongao/phoenix/shiren/controller/IndexController.java
@@ -33,4 +33,14 @@ public class IndexController {
         result.put("name", "小明");
         return result;
     }
+
+    @RequestMapping(value = "/test/third", produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public Map<String,Object> third() {
+        Map<String,Object> result = new HashMap<>();
+        result.put("age", 18);
+        result.put("sex", "男");
+        result.put("name", "小明");
+        return result;
+    }
 }
